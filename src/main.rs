9683749d53d5641afe4351adeeac13f75eeca39a
Yannick Guern <dev@guern.eu>
2025-08-30 19:53:48 +0200

    doc: Comment main.rs
    Use toaster to generate documentation, because I'm lazy. :p

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+//! Entry point of the program.
+//!
+//! The `main` function is the starting point of any Rust application.
+//! When the program is executed, this function is called.
+//! In this example, it prints "Hello, world!" to the console.
+//!
+//! # Example
+//! ```
+//! $ cargo run
+//! Hello, world!
+//! ```
+/// The entry point of the Rust program.
+///
+/// This `main` function serves as the starting point for the execution of the program.
+/// When executed, it prints the message "Hello, world!" to the standard output.
+///
+/// # Example
+///
+/// ```
+/// // This will print "Hello, world!" to the console.
+/// fn main() {
+///     println!("Hello, world!");
+/// }
+/// ```
 fn main() {
     println!("Hello, world!");
 }
